98088d4e0926115dc36f2dd67108f6543a993a6b
yydongsq <yydongsq@126.com>
2021-08-03 17:40:20 +0800

    hot-fix-2021-08-03

--- src/com/sq/LeetCode1.java
+++ src/com/sq/LeetCode1.java
@@ -6,6 +6,7 @@ package com.sq;
  */
 public class LeetCode1 {
     public static void main(String[] args) {
-        System.out.println("hello Git!");
+        System.out.println("hello Git1!");
+        System.out.println("hello Git2!");
     }
 }
