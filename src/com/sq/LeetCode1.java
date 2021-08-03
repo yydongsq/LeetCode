064a7278b04fd5077df2068f733e8b077563a93d
yydongsq <yydongsq@126.com>
2021-08-03 17:50:33 +0800

    master-2021-08-03-2

--- src/com/sq/LeetCode1.java
+++ src/com/sq/LeetCode1.java
@@ -8,5 +8,6 @@ public class LeetCode1 {
     public static void main(String[] args) {
         System.out.println("hello Git1!");
         System.out.println("hello Git2!");
+        System.out.println("hello Git4!");
     }
 }
